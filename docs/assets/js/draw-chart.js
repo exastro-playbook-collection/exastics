e9a874eb1289e9aa23f94810a25483a1db4edf4a
Daisuke <57690977+dai-kagino@users.noreply.github.com>
2021-04-20 20:30:43 +0900

    Update draw-chart.js

--- docs/assets/js/draw-chart.js
+++ docs/assets/js/draw-chart.js
@@ -11,7 +11,7 @@ var insertLabelPlugin = {
 
             context.fillStyle = 'rgb(0, 0, 0)';
             context.font = Chart.helpers.fontString(16, 'normal', 'Arial');
-            context.fillText('Total: ' + total_count.toString(), 20, 0);
+            context.fillText('Total: ' + total_count.toString(), 20, 30);
         });
     }
 };
