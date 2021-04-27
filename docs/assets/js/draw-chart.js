887097de3a0025c21cccc06f524bef81ae256485
Daisuke <57690977+dai-kagino@users.noreply.github.com>
2021-04-27 23:24:26 +0900

    Update draw-chart.js

--- docs/assets/js/draw-chart.js
+++ docs/assets/js/draw-chart.js
@@ -198,7 +198,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(chartData => {
                     console.log(chartData)
                     const context = document.getElementById("chart-canvas-" + 0);
-                    attachBarChart(context, chartData)
+                    attachBarChart(context, barData)
                 });
         });
 });
